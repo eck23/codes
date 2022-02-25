0ad1f53ddafc5d91d94560334e9795dc60104044
Elson C Kaithamangalam <62635566+eck23@users.noreply.github.com>
2022-02-25 22:31:57 +0530

    Add files via upload

--- absloader.c
+++ absloader.c
@@ -7,7 +7,7 @@ void main(){
     FILE *objcode,*tempFile;
     objcode=fopen("objcode.txt","r");
 
-    char record[200],address[10],temp[10];
+    char record[200],address[10];
     int i,start,count=0,j=0,change=0;
     fscanf(objcode,"%s",record);
   
@@ -20,13 +20,13 @@ void main(){
         if(count==3)
             break;
     }
-    // scanf("%x",&start);
-    tempFile=fopen("tempFile.txt","w");
-	fprintf(tempFile,"%s",address);
-	fclose(tempFile);
-	tempFile=fopen("tempFile.txt","r");
-	fscanf(tempFile,"%x",&start);
-	fclose(tempFile);
+    scanf("%x",&start);
+    // tempFile=fopen("tempFile.txt","w");
+	// fprintf(tempFile,"%s",address);
+	// fclose(tempFile);
+	// tempFile=fopen("tempFile.txt","r");
+	// fscanf(tempFile,"%x",&start);
+	// fclose(tempFile);
     fscanf(objcode,"%s",record);
     
     printf("adress\tobjectCode",record);
